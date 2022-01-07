7326a89169b4edf29075eeb621df9f1f6a12b084
Axure <96837058+Axure0@users.noreply.github.com>
2022-01-07 20:26:15 +0000

    Update index.js

--- DJS-V13/code-samples/Advanced-Bot/index.js
+++ DJS-V13/code-samples/Advanced-Bot/index.js
@@ -1,9 +1,8 @@
 const { Client, Intents } = require('discord.js');
 
-const allIntents = Intents.all();
 const { token, prefix } = require('./config.json');
 
-const client = new Client({ intents: allIntents });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', (client) =>{
   console.log(`Logged in as ${client.user.tag}`);
