a0bc7aa60de9f4d7b12ef23c85f1ed13da29c50a
Axure <96837058+Axure0@users.noreply.github.com>
2022-01-08 11:20:30 +0000

    Update index.js

--- DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/index.js
+++ DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/index.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const fs = require('fs')
 const { token, prefix } = require('./config.json');
 
@@ -13,17 +13,11 @@ const client = new Client({
  * Using 'fs' node Module to read the Events folder
  * Then filtering out the .js Files
  */
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('js'))
-for (const file of eventFiles) { //Making a Loop for Each of the File
-  const event = require(`../events/${file}`); //Loading the File
-  if (event.name) { //So that it doesnt crash
-    if (event.disabled) continue; //If we want to disable the event, we can use 'disabled: true' in the event config
-    if (event.once) { //The difference between once and on is, once runs only for one time, on can be used infinitely
-      client.once(event.name, (...args) => event.execute(...args, client)); // Requiring the params, as they all have different parameters
-    } else {
-      if (event.disabled) continue; //Why not check it again
-      client.on(event.name, (...args) => event.execute(...args, client)); //Doing the same thing, but instead of 'client.once', we used 'client.on'
-    }
-  }
-}
+
+client.commands = new Collection();
+
+['command_handler', 'event_handler'].forEach(handler =>{
+  require(`./handlers/${handler}`)(client, Discord)
+});
+
 client.login(token)
