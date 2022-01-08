75029ca699de377395e8c307e57b5cd1dffaafed
Axure <96837058+Axure0@users.noreply.github.com>
2022-01-08 11:16:00 +0000

    Delete DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/events directory

--- DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/events/ready.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-module.exports = {
-  name: "ready", //This will be used as 'client.on('ready', () => {})'
-  description: "When the Bot starts up", //Just to be user friendly, this isnt used ANYWHERE
-  once: true, //Because the ready event needs to be fired once only
-  execute(client) {
-    console.log(`Ready!\nLogged in as ${client.user.tag}`)
-  }
-}
