c251e17f3f96e9616b2acfff935d98a6361ab02e
Axure <96837058+Axure0@users.noreply.github.com>
2022-02-06 13:25:57 +0000

    Update command_handler.js

--- DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/handlers/command_handler.js
+++ DJS-V13/code-samples/Bot Tutorials/Advanced-Bot/handlers/command_handler.js
@@ -5,10 +5,12 @@ module.exports = (client, Discord) => {
 
   for (const file of commandFiles) {
     const command = require(`../commands/${file}`);
+    let n = file.split(".")[0]
+    let files = n.replace(".js", "");
     if (command.name) {
       client.commands.set(command.name, command);
+      console.log(`${files} Was Loaded!`)
     } else {
-      const n = file.split(".")[0]
       console.log(`${n} was not loaded because the command Name is missing!`)
       continue;
     }
